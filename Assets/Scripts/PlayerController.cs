ac044ab6520414170cd84d4c8a9e8229687e31e3
Claire Irou <c.iroudayassamy@gmail.com>
2019-09-02 05:28:28 -0400

    Player Input
    Worked on the script and now have player controlled movement. Very cool.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void FixedUpdate ()
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
     {
-        Input
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
